313593824835583e46a5e50d10e785713e0a8c69
franco <francozacco@gmail.com>
2024-07-15 16:18:15 +0200

    updating version

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -8,7 +8,7 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "0.1.24"
+VERSION = "0.1.25"
 def readme():
     try:
         with open('README.md', encoding='utf-8') as f:
